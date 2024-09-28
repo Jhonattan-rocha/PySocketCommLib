8239f66dd0378bba23e2d8ebe98225e2d38fea7b
Jhonattan Rocha da Silva <97904111+Jhonattan-rocha@users.noreply.github.com>
2024-09-28 19:39:00 -0300

    teste

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,2 +1,2 @@
-from Server.asyncser.server import Server as AsyncServer
-from Server.thread.server import Server as ThreadServer
+from Server.asyncserv.server import Server as AsyncServer
+from Server.threadserv.server import Server as ThreadServer
